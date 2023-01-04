f9b18e4c4327b41174e8219061bc4fdd4551d8ad
Khaled Essam <khaledx30@gmail.com>
2023-01-04 23:19:15 +0200

    test3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -3,7 +3,7 @@ import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar";
 // git test sadfsdaf
-//heloo
+
 function App() {
   const [formOpen, setFormOpen] = useState(false);
   return (
