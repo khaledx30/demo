5807f86bca61dfc34a291054b328be0a11fb792c
Khaled Essam <khaledx30@gmail.com>
2023-01-05 10:45:26 +0200

    Update App.jsx
    new line

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar";
-// here is comment from git hub rebo
+// here is comment from git hub rebo lol
 function App() {
   const [formOpen, setFormOpen] = useState(false);
   return (
